db1adae8249b75d7795f5344b28e9d7880963ddf
Steve Alex <steve.alex@hotmail.co.uk>
2021-01-24 16:38:39 +0000

    Return coordinates from postcodes API

--- app.ts
+++ app.ts
@@ -3,28 +3,51 @@ const axios = require('axios').default;
 
 const app = express();
 
+interface Coordinates {
+    postcode: string,
+    longitude: number,
+    latitude: number,
+}
 
-
-app.get("/postcode/:postcode", (req, res) => {
+app.get("/postcode/:postcode", (req: express.Request, res: express.Response) => {
     const postcodes = req.params.postcode.split(",");
 
     validatePostcodes(postcodes);
 
+    getCoordinatesFromPostcodes(postcodes)
+        .then(coordinatesData => console.log(coordinatesData))
+        .catch(error => console.log(error))
 });
+
+export async function getCoordinatesFromPostcodes(postcodes: any) {
+    return axios.post(`https://api.postcodes.io/postcodes/`, { postcodes })
+        .then((response: any) => {
+            const coordinates: Coordinates = response.data.result.map((location: any) => {
+                return {
+                    postcode: location.result.postcode,
+                    longitude: location.result.longitude,
+                    latitude: location.result.latitude
+                }
+            })
+
+            return coordinates;
+        })
+        .catch((error: any) => {
+            throw new Error("Unable to fetch coordinates from postcodes")
+        })
+}
 
 const validatePostcodes = (postcodes: string[]) => {    
     var postcodeRegEx = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/i; 
 
     for (const postcode of postcodes){
         if (!postcodeRegEx.test(postcode)){
-            throw new Error("Not a valid UK postcode")
+            throw new Error("Not a valid UK postcode");
         }
     }
 }
 
-export async function getLatlonFromPostcode(postcode: any) {
-    return "51.5074, 0.1278";
-}
+
 
 export default app;
 
