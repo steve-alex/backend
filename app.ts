4e4a195348ff8348bc40500e421d53926ae52be4
Steve Alex <steve.alex@hotmail.co.uk>
2021-01-24 16:13:25 +0000

    Added Postcode Validation

--- app.ts
+++ app.ts
@@ -1,13 +1,26 @@
 import * as express from "express";
+const axios = require('axios').default;
 
 const app = express();
 
+
+
 app.get("/postcode/:postcode", (req, res) => {
-    const postcode = req.params.postcode;
-    getLatlonFromPostcode(postcode).then(data => {
-        res.send(data);
-    });
+    const postcodes = req.params.postcode.split(",");
+
+    validatePostcodes(postcodes);
+
 });
+
+const validatePostcodes = (postcodes: string[]) => {    
+    var postcodeRegEx = /[A-Z]{1,2}[0-9]{1,2} ?[0-9][A-Z]{2}/i; 
+
+    for (const postcode of postcodes){
+        if (!postcodeRegEx.test(postcode)){
+            throw new Error("Not a valid UK postcode")
+        }
+    }
+}
 
 export async function getLatlonFromPostcode(postcode: any) {
     return "51.5074, 0.1278";
